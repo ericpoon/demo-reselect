224336a6b7ded8eab9307b60289c63dc0e7c7f6a
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-25 15:34:20 +0800

    Add test specifications

---
+++ tests/reselect.test.js
@@ -0,0 +1,7 @@
+describe('returns the cached output if input does not change', () => {
+  it('accepts one pre-selector and one final selector');
+  it('accepts more than one pre-selectors');
+  it('accepts only one selector');
+  it('accepts pre-selectors returning undefined');
+  it('accepts undefined as the argument of the created selector');
+});
