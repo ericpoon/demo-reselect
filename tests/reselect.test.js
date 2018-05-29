05b4e1a9ff061dda9a27dc2b8b61acb01d608007
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-29 15:04:06 +0800

    Renaming

--- tests/reselect.test.js
+++ tests/reselect.test.js
@@ -1,88 +1,88 @@
 import { createSelector } from '../src/reselect';
 
 it('accepts only one selector', () => {
-  const preSelector = jest.fn((x) => x * 2);
-  const selector = createSelector(preSelector);
+  const inputSelector = jest.fn((x) => x * 2);
+  const selector = createSelector(inputSelector);
 
   expect(selector(1)).toBe(undefined);
-  expect(preSelector).toHaveBeenCalledTimes(0);
+  expect(inputSelector).toHaveBeenCalledTimes(0);
 });
 
-it('accepts pre-selectors returning undefined', () => {
-  const preSelector = jest.fn(() => undefined);
+it('accepts input selectors returning undefined', () => {
+  const inputSelector = jest.fn(() => undefined);
   const outputSelector = jest.fn(() => 100);
-  const selector = createSelector(preSelector, outputSelector);
+  const selector = createSelector(inputSelector, outputSelector);
 
   expect(selector()).toBe(100);
 });
 
 describe('recalculates if input changes', () => {
   it('recalculates if the only input changes', () => {
-    const preSelector = jest.fn((x) => x * 2);
+    const inputSelector = jest.fn((x) => x * 2);
     const outputSelector = jest.fn((x) => x / 2);
-    const selector = createSelector(preSelector, outputSelector);
+    const selector = createSelector(inputSelector, outputSelector);
 
     expect(selector(1)).toBe(1);
-    expect(preSelector).toHaveBeenCalledTimes(1);
+    expect(inputSelector).toHaveBeenCalledTimes(1);
     expect(outputSelector).toHaveBeenCalledTimes(1);
 
     expect(selector(2)).toBe(2);
-    expect(preSelector).toHaveBeenCalledTimes(2);
+    expect(inputSelector).toHaveBeenCalledTimes(2);
     expect(outputSelector).toHaveBeenCalledTimes(2);
   });
   it('recalculates if one of the inputs changes', () => {
-    const preSelectorA = jest.fn((x) => x * x);
-    const preSelectorB = jest.fn((x) => x * x * x * x);
-    const preSelectorC = jest.fn((x) => x + 1);
+    const inputSelectorA = jest.fn((x) => x * x);
+    const inputSelectorB = jest.fn((x) => x * x * x * x);
+    const inputSelectorC = jest.fn((x) => x + 1);
     const outputSelector = jest.fn((x, y, z) => x + y + z);
-    const selector = createSelector(preSelectorA, preSelectorB, preSelectorC, outputSelector);
+    const selector = createSelector(inputSelectorA, inputSelectorB, inputSelectorC, outputSelector);
 
     expect(selector(2)).toBe(23);
-    expect(preSelectorA).toHaveBeenCalledTimes(1);
-    expect(preSelectorB).toHaveBeenCalledTimes(1);
-    expect(preSelectorC).toHaveBeenCalledTimes(1);
+    expect(inputSelectorA).toHaveBeenCalledTimes(1);
+    expect(inputSelectorB).toHaveBeenCalledTimes(1);
+    expect(inputSelectorC).toHaveBeenCalledTimes(1);
     expect(outputSelector).toHaveBeenCalledTimes(1);
 
     expect(selector(-2)).toBe(19);
-    expect(preSelectorA).toHaveBeenCalledTimes(2);
-    expect(preSelectorB).toHaveBeenCalledTimes(2);
-    expect(preSelectorC).toHaveBeenCalledTimes(2);
+    expect(inputSelectorA).toHaveBeenCalledTimes(2);
+    expect(inputSelectorB).toHaveBeenCalledTimes(2);
+    expect(inputSelectorC).toHaveBeenCalledTimes(2);
     expect(outputSelector).toHaveBeenCalledTimes(2);
   });
 });
 
 describe('returns the cached output if input does not change', () => {
   it('accepts one pre-selector and one output selector', () => {
-    const preSelector = jest.fn((x) => x * 2);
+    const inputSelector = jest.fn((x) => x * 2);
     const outputSelector = jest.fn((x) => x / 2);
-    const selector = createSelector(preSelector, outputSelector);
+    const selector = createSelector(inputSelector, outputSelector);
 
     expect(selector(1)).toBe(1);
-    expect(preSelector).toHaveBeenCalledTimes(1);
+    expect(inputSelector).toHaveBeenCalledTimes(1);
     expect(outputSelector).toHaveBeenCalledTimes(1);
 
     expect(selector(1)).toBe(1);
-    expect(preSelector).toHaveBeenCalledTimes(2);
+    expect(inputSelector).toHaveBeenCalledTimes(2);
     expect(outputSelector).toHaveBeenCalledTimes(1);
   });
 
-  it('accepts more than one pre-selectors', () => {
-    const preSelectorA = jest.fn((x) => x * 2);
-    const preSelectorB = jest.fn((x) => x * 3);
-    const preSelectorC = jest.fn((x) => x * 5);
+  it('accepts more than one input selectors', () => {
+    const inputSelectorA = jest.fn((x) => x * 2);
+    const inputSelectorB = jest.fn((x) => x * 3);
+    const inputSelectorC = jest.fn((x) => x * 5);
     const outputSelector = jest.fn((x, y, z) => x + y + z);
-    const selector = createSelector(preSelectorA, preSelectorB, preSelectorC, outputSelector);
+    const selector = createSelector(inputSelectorA, inputSelectorB, inputSelectorC, outputSelector);
 
     expect(selector(1)).toBe(10);
-    expect(preSelectorA).toHaveBeenCalledTimes(1);
-    expect(preSelectorB).toHaveBeenCalledTimes(1);
-    expect(preSelectorC).toHaveBeenCalledTimes(1);
+    expect(inputSelectorA).toHaveBeenCalledTimes(1);
+    expect(inputSelectorB).toHaveBeenCalledTimes(1);
+    expect(inputSelectorC).toHaveBeenCalledTimes(1);
     expect(outputSelector).toHaveBeenCalledTimes(1);
 
     expect(selector(1)).toBe(10);
-    expect(preSelectorA).toHaveBeenCalledTimes(2);
-    expect(preSelectorB).toHaveBeenCalledTimes(2);
-    expect(preSelectorC).toHaveBeenCalledTimes(2);
+    expect(inputSelectorA).toHaveBeenCalledTimes(2);
+    expect(inputSelectorB).toHaveBeenCalledTimes(2);
+    expect(inputSelectorC).toHaveBeenCalledTimes(2);
     expect(outputSelector).toHaveBeenCalledTimes(1);
   });
 });
