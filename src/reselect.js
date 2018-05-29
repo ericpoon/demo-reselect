8c13f4a800b7edc896a7c9bc6057c9c10a73bea3
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-29 14:56:12 +0800

    Add createSelectorCreator

--- src/reselect.js
+++ src/reselect.js
@@ -1,39 +1,48 @@
-export function createSelector(...selectors) {
-  let prevInput = [];
-  let prevOutput;
-  return function (...args) {
-    const input = [];
-    for (let i = 0; i < selectors.length - 1; i++) {
-      input.push(selectors[i](...args));
-    }
+export function createSelectorCreator(isEqual) {
 
-    let inputUnchanged = true;
+  function createSelector(...selectors) {
+    let prevInput = [];
+    let prevOutput;
+    return function (...args) {
+      const input = [];
+      for (let i = 0; i < selectors.length - 1; i++) {
+        input.push(selectors[i](...args));
+      }
 
-    if (input.length !== prevInput.length) {
-      inputUnchanged = false;
-    } else {
-      for (let i = 0; i < input.length; i++) {
-        if (!referenceEqual(input[i], prevInput[i])) {
-          inputUnchanged = false;
-          break;
+      let inputUnchanged = true;
+
+      if (input.length !== prevInput.length) {
+        inputUnchanged = false;
+      } else {
+        for (let i = 0; i < input.length; i++) {
+          if (!isEqual(input[i], prevInput[i])) {
+            inputUnchanged = false;
+            break;
+          }
         }
       }
-    }
 
-    if (!inputUnchanged) {
-      const outputSelector = selectors[selectors.length - 1];
-      prevInput = input;
-      prevOutput = outputSelector(...input);
-    }
-    return prevOutput;
-  };
+      if (!inputUnchanged) {
+        const outputSelector = selectors[selectors.length - 1];
+        prevInput = input;
+        prevOutput = outputSelector(...input);
+      }
+      return prevOutput;
+    };
+  }
+
+  return createSelector;
 }
 
-function referenceEqual(a, b) {
+export function createSelector(...selectors) {
+  return createSelectorCreator(referenceEqual)(...selectors);
+}
+
+export function referenceEqual(a, b) {
   return a === b;
 }
 
-function shallowObjectEqual(a, b) {
+export function shallowObjectEqual(a, b) {
   if (typeof a === 'object' && typeof b === 'object') {
     const aKeys = Object.keys(a);
     const bKeys = Object.keys(b);
